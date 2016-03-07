d447656264d7812a0126641821b2e33c2979dd01
beymarr <beymar.ruidiaz.m@gmail.com>
2016-03-07 17:55:20 -0500

    test1 para  dos puertos con un atmega 8515, en donde un puerto se declara como entrada y el otro como salida, es decir que un puerto va a leer va a escribir lo que tiene

---
+++ TEST1_DOSPUERTOS/TEST_1codeblocks/main.c
@@ -0,0 +1,17 @@
+#include "procesador.h"
+
+int main(){
+
+    uint8_t value;
+
+    CPUInit();
+
+    CPUWrite(0x2000, 0x00);
+    CPUWrite(0x4000, 0xFF);
+
+    while(1){
+        value = CPURead(0x2002);
+        CPUWrite(0x4001, value);
+        _delay_ms(10);
+    }
+}
